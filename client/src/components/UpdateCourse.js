9e1e6e66e9665f732102b137b048e086396e259b
karinabolat <90366213+karinabolat@users.noreply.github.com>
2022-01-09 04:33:21 -0600

    Update UpdateCourse.js

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -77,32 +77,33 @@ const UpdateCourse = () => {
     
     return (
     <main>
-        {course.userId !== context.authenticatedUser.id? navigate('/forbidden') : null }
-        <div className="wrap">
-            <h2>Update Course</h2>
-                <ErrorsDisplay errors={errors} />
-                <form onSubmit={submit}>
-                    <div className="main--flex">
-                        <div>
-                            <label htmlFor="courseTitle">Course Title</label>
-                            <input id="courseTitle" name="courseTitle" type="text" value={title} onChange={change} />
+        {course.userId === context.authenticatedUser.id? (
+            <div className="wrap">
+                <h2>Update Course</h2>
+                    <ErrorsDisplay errors={errors} />
+                    <form onSubmit={submit}>
+                        <div className="main--flex">
+                            <div>
+                                <label htmlFor="courseTitle">Course Title</label>
+                                <input id="courseTitle" name="courseTitle" type="text" value={title} onChange={change} />
 
-                            {course.User? <p>By {course.User.firstName} {course.User.lastName}</p> : null}
+                                {course.User? <p>By {course.User.firstName} {course.User.lastName}</p> : null}
 
-                            <label htmlFor="courseDescription">Course Description</label>
-                            <textarea id="courseDescription" name="courseDescription" onChange={change} value={description} />
+                                <label htmlFor="courseDescription">Course Description</label>
+                                <textarea id="courseDescription" name="courseDescription" onChange={change} value={description} />
+                            </div>
+                            <div>
+                                <label htmlFor="estimatedTime">Estimated Time</label>
+                                <input id="estimatedTime" name="estimatedTime" type="text" value={estimatedTime} onChange={change}/>
+
+                                <label htmlFor="materialsNeeded">Materials Needed</label>
+                                <textarea id="materialsNeeded" name="materialsNeeded" onChange={change} value={materialsNeeded} />
+                            </div>
                         </div>
-                        <div>
-                            <label htmlFor="estimatedTime">Estimated Time</label>
-                            <input id="estimatedTime" name="estimatedTime" type="text" value={estimatedTime} onChange={change}/>
-
-                            <label htmlFor="materialsNeeded">Materials Needed</label>
-                            <textarea id="materialsNeeded" name="materialsNeeded" onChange={change} value={materialsNeeded} />
-                        </div>
-                    </div>
-                    <button className="button" type="submit">Update Course</button><button className="button button-secondary" onClick={cancel}>Cancel</button>
-                </form>
-        </div>
+                        <button className="button" type="submit">Update Course</button><button className="button button-secondary" onClick={cancel}>Cancel</button>
+                    </form>
+            </div>
+        ) : navigate('/forbidden') }
     </main>
     );
 }
